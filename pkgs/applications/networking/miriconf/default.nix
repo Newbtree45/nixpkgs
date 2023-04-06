63b86d808baf3de6d95ccc5c7b10c784f55f48d1
Travis Newberry <newberts@mail.uc.edu>
2023-04-05 23:22:59 -0400

    Getting Closer

--- pkgs/applications/networking/miriconf/default.nix
+++ pkgs/applications/networking/miriconf/default.nix
@@ -10,6 +10,9 @@ buildGoModule rec {
     rev = "b6536863bb32b4e3640a5ccbc23309f19ace5c4f";
     sha256 = "SHA256-079136F606C3F5F2E840E22A20D52EDE9C6323F1B43C740315B18DCB4521101F";
   };
+  
+  vendorHash = null;
+
 
   meta = with lib; {
     description = "An agent for miriconf used to manage multiple devices over a network.";

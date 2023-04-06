5d8ce26a7fcadbbb139bdc8083514e96e14d6e23
Travis Newberry <newberts@mail.uc.edu>
2023-04-05 23:37:21 -0400

    Fixed Rev

--- pkgs/applications/networking/miriconf/default.nix
+++ pkgs/applications/networking/miriconf/default.nix
@@ -7,7 +7,7 @@ buildGoModule rec {
   src = fetchFromGitHub {
     owner = "MiriConf";
     repo = "miriconf-agent";
-    rev = "b6536863bb32b4e3640a5ccbc23309f19ace5c4f";
+    rev = "rf4t9NhQBgPjn06yAlziTogAOoCP0RVY8NOo3u/dREQ=";
     sha256 = "079136F606C3F5F2E840E22A20D52EDE9C6323F1B43C740315B18DCB4521101F";
   };
   

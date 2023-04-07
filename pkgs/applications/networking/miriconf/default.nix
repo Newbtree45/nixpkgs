8ebc9cf475e5ac95ad12884dad83ce7ad03286db
Travis Newberry <newberts@mail.uc.edu>
2023-04-07 09:11:32 -0400

    Update default.nix

--- pkgs/applications/networking/miriconf/default.nix
+++ pkgs/applications/networking/miriconf/default.nix
@@ -4,9 +4,11 @@ buildGoModule rec {
   pname = "miriconf";
   version = "0.5";
 
-  src = fetchzip {
-   url="https://github.com/MiriConf/miriconf-agent.git";
-    sha256 = "079136F606C3F5F2E840E22A20D52EDE9C6323F1B43C740315B18DCB4521101F";
+   src = fetchFromGitHub {
+    owner = "MiriConf";
+    repo = "miriconf-agent";
+    rev = "b6536863bb32b4e3640a5ccbc23309f19ace5c4f";
+    sha256 = "SHA256-079136F606C3F5F2E840E22A20D52EDE9C6323F1B43C740315B18DCB4521101F";
   };
   
   vendorHash = null;

c883a9bea149eeed6ba05c17304858fe638b4e8f
Travis Newberry <newberts@mail.uc.edu>
2023-04-09 19:00:24 -0400

    Update default.nix

--- pkgs/applications/networking/miriconf/default.nix
+++ pkgs/applications/networking/miriconf/default.nix
@@ -1,7 +1,6 @@
 with import <nixpkgs> {};
 
 stdenv.mkDerivation rec {
-buildGoModule rec {
   pname = "miriconf-agent";
   version = "1.19";
 
@@ -12,12 +11,10 @@ buildGoModule rec {
     sha256 = "rf4t9NhQBgPjn06yAlziTogAOoCP0RVY8NOo3u/dREQ=";
   };
   
-   vendorHash = "sha256-PfJNr7t/27PSnwIwFv0kHV3f+er0fpHwqddS8yS7ofo=";
-
+ enableParallelBuilding = true;
 
   meta = with lib; {
     description = "An agent for miriconf used to manage multiple devices over a network.";
     homepage = "https://github.com/orgs/MiriConf/repositories";
   };
-}
 }
